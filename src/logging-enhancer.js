81319a41190864179d9d9167b0f73a4f69a9a8fd
bbottema <b.bottema@projectnibble.org>
2015-05-26 18:05:51 +0000

    fixing Code Climate issues

--- src/logging-enhancer.js
+++ src/logging-enhancer.js
@@ -29,7 +29,7 @@
 						if (config.logLevels[context] !== undefined) {
 							return config.logLevels[context];
 						}
-						else if (context.indexOf('.') != -1) {
+						else if (context.indexOf('.') !== -1) {
 							return getLogLevelThreshold(context.substring(0, context.lastIndexOf('.')), config);
 						}
 					}
@@ -102,7 +102,7 @@
 			// these numbers are irrelevant, could be anything unique
 			sprintf(pattern, f(1), f(2), f(3), f(4), f(5), f(6), f(7), f(8), f(9), f(10));
 			return placeholderCounter;
-		}
+		};
 	};
 
 	if (typeof window !== "undefined") {
