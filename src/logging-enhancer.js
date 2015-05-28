d8135beb7a240915913ebe2a452d3798209c7e79
Benny Bottema <b.bottema@projectnibble.org>
2015-05-28 17:18:55 +0200

    fixed comment

--- src/logging-enhancer.js
+++ src/logging-enhancer.js
@@ -100,7 +100,6 @@
 				};
 			}
 
-			// these numbers are irrelevant, could be anything unique
 			sprintf(pattern, f(1), f(2), f(3), f(4), f(5), f(6), f(7), f(8), f(9), f(10));
 			return placeholderCounter;
 		};
