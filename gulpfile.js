a1b9c8745e18f630cd20e973173b523af83a7479
bbottema <b.bottema@projectnibble.org>
2015-05-29 20:36:32 +0000

    switched to browserify and started relying on better-logging-base

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,33 @@ var jasmine = require('gulp-jasmine');
 var cover = require('gulp-coverage');
 var coveralls = require('gulp-coveralls');
 var lazypipe = require('lazypipe');
+var browserify = require('browserify');
+var transform = require('vinyl-transform');
+var buffer = require('vinyl-buffer');
+var size = require('gulp-size');
+var source = require('vinyl-source-stream');
 
 gulp.task('clean', function() {
     del(['dist/*', 'reports', 'debug', '.coverdata']);
 });
 
-gulp.task('build', ['clean'], function() {
-    gulp.src(['src/logging-enhancer.js', 'src/angular-logger.js'])
+gulp.task('browserify', function () {
+  return browserify({entries: ['./src/angular-logger.js']}).bundle()
+    .pipe(source('angular-logger-browserified.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./debug/'));
+});
+
+gulp.task('build', ['clean', 'browserify'], function() {
+    return gulp.src(['debug/**/*.js'])
+        .pipe(concat("angular-logger.js"))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('dist', ['build'], function() {
+    return gulp.src(['debug/**/*.js'])
         .pipe(uglify())
-        .pipe(concat("logger.min.js"))
+        .pipe(concat("angular-logger.min.js"))
         .pipe(gulp.dest('dist'));
 });
 
@@ -23,23 +41,25 @@ var testAndGather = lazypipe()
         pattern: ['src/**/*.js'],
         debugDirectory: 'debug'
     })
-    .pipe(jasmine, {includeStackTrace: true})
+    .pipe(jasmine, {
+        includeStackTrace: true
+    })
     .pipe(cover.gather);
 
-gulp.task('test', ['build'], function () {
+gulp.task('test', ['build'], function() {
     gulp.src('spec/**/*spec.js')
         .pipe(testAndGather())
         .pipe(cover.format(['html']))
         .pipe(gulp.dest('reports'));
 });
 
-gulp.task('travis', ['build'], function () {
+gulp.task('travis', ['build'], function() {
     gulp.src('spec/**/*spec.js')
         .pipe(testAndGather())
         .pipe(cover.format(['lcov']))
         .pipe(coveralls());
 });
 
-gulp.task('default', ['build'], function() {
+gulp.task('default', ['dist'], function() {
     // place code for your default task here
 });
